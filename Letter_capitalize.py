a74d19ac3886ac7ec5b603996e73dc2689e724ec
Neha Vaidya <74299112+neha290694@users.noreply.github.com>
2021-09-18 15:30:47 +0530

    Update Letter_capitalize.py

--- Letter_capitalize.py
+++ Letter_capitalize.py
@@ -7,14 +7,3 @@ def LetterCapitalize(s):
 s = input("Input:")
 t = LetterCapitalize(s)
 print(t)
-
-
-'''
-num=int(input("Enter a number:"))
-for i in range(1,num+1):
-    print(" "*(i-1),end="")
-    for j in range(1,num+2-i):
-        print("*",end=" ")
-    for k in range(1,num+1-i):
-        print("*",end=" ")
-    print()'''
