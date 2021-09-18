8801c20561b98e61c1b60b887005879090d88724
Neha Vaidya <74299112+neha290694@users.noreply.github.com>
2021-09-18 14:16:38 +0530

    Add files via upload

---
+++ camelcase.py
@@ -0,0 +1,16 @@
+import re
+
+class Solution:
+   def CamelCase(self, str):
+      s = "".join(word[0].upper() + word[1:].lower() for word in str)
+      return s[0].lower() + s[1:]
+
+   def main():
+      ob = Solution()
+      string = input("Input:")
+      words = re.sub("[^0-9a-zA-Z']+", ' ', string)
+      str = words.split(' ')
+      
+      print("Output:",ob.CamelCase(str))
+      
+Solution.main()
